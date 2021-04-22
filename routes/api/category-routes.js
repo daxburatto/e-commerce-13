b7a541a77beacde32f618b9520065a3aaf7bff1d
Dax Buratto <daxburatto@gmail.com>
2021-04-21 20:46:57 -0500

    created db

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,6 +28,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 router.put('/:id', (req, res) => {
