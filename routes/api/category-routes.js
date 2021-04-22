affef805615fbf390368e5bf06726499b873a7fb
Dax Buratto <daxburatto@gmail.com>
2021-04-21 19:46:36 -0500

    set up models and some api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,24 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product]
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 router.post('/', (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+      include: Product,
+      through: ProductTag
+    }, ],
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 router.get('/:id', (req, res) => {
