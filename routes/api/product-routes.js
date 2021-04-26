25f6b25d0829d0ad1885e6879a691383077791ef
Dax Buratto <daxburatto@gmail.com>
2021-04-26 16:37:22 -0500

    finished tag-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,7 +50,14 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    id:req.body.id,
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tag_name: req.body.tag_name
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -75,12 +82,20 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
-    where: {
+  Product.update(
+    {
       id: req.params.id,
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tag_name: req.body.tag_id,
     },
-  })
-    .then((product) => {
+    {
+      where: {
+        id: req.params.id
+      },
+    }).then((product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
