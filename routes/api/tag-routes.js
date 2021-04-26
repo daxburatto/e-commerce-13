13c6c23c7f801f6ea6c4e23683365316002da7e4
Dax Buratto <daxburatto@gmail.com>
2021-04-26 16:46:15 -0500

    added put and delete routes to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,10 +41,28 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    id: req.body.id,
+    tag_name: req.body.tag_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 module.exports = router;
